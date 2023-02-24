dceb53ceeefea14dfdf5d3c72895a8e1664ee159
Mikhail Abakumov <abakumovms7@yandex.ru>
2023-02-24 14:42:21 +0500

    1/2 hw

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import render, reverse
 
 DATA = {
     'omlet': {
@@ -16,15 +17,36 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def home_view(request):
+    template_name = 'calculator/home.html'
+    pages = {
+        'Рецепт приготовления омлета': reverse('omlet'),
+        'Рецет приготовления пасты': reverse('pasta'),
+        'Рецепт приготовления бутерброда': reverse('buter')
+    }
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def get_omlet(request):
+    servings = int(request.GET.get('servings', 1))
+    context = {}
+    context.setdefault('recipe', {ing : count * servings for ing, count in DATA['omlet'].items()})
+    return render(request, 'calculator/index.html', context)
+
+def get_pasta(request):
+    servings = int(request.GET.get('servings', 1))
+    context = {}
+    context.setdefault('recipe', {ing : count * servings for ing, count in DATA['pasta'].items()})
+    return render(request, 'calculator/index.html', context)
+
+
+def get_buter(request):
+    servings = int(request.GET.get('servings', 1))
+    context = {}
+    context.setdefault('recipe', {ing : count * servings for ing, count in DATA['buter'].items()})
+    return render(request, 'calculator/index.html', context)
--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 
 from django.urls import path
 
+from calculator.views import get_buter, get_omlet, get_pasta, home_view
+
+
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('', home_view, name='home'),
+    path('omlet/', get_omlet, name='omlet'),
+    path('pasta/', get_pasta, name='pasta'),
+    path('buter/', get_buter, name='buter'),
+    
 ]
