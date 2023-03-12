cea467c427baf830643c7306586a4aa92a7116c8
Mikhail Abakumov <abakumovms7@yandex.ru>
2023-03-12 14:06:13 +0500

    bd hw2 fix

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -6,20 +6,18 @@ from books.models import Book
 
 def books_view(request):
     template = 'books/books_list.html'
-
-    books_object = list(Book.objects.values())
-    for i in books_object:
-        i['pub_date'] = i['pub_date'].strftime('%Y-%m-%d')
+    books_object = Book.objects.all()
     context = {'books': books_object}
     return render(request, template, context)
 
 def date_view(request, pub_date):
-    template = 'books/book.html'
-    books_object = list(Book.objects.values())
-    for i in books_object:
-        i['pub_date'] = i['pub_date'].strftime('%Y-%m-%d')
-    paginator = Paginator(books_object, 1)
-    page = paginator.get_page(pub_date)
-    context= {'books': [i for i in books_object if i['pub_date'] == pub_date],
-             'page': page}
+    template = 'books/books_list.html'
+    books_objects = Book.objects.filter(pub_date=pub_date)
+    books_next = Book.objects.filter(pub_date__gt=pub_date).values('pub_date').first()
+    books_previous = Book.objects.filter(pub_date__lt=pub_date).values('pub_date').first()
+    context = {
+        'books': books_objects,
+        'next_book': books_next,
+        'previous_book': books_previous,
+    }
     return render(request, template, context)
